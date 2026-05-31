940c517d17c704e08167ad22092a0006877a37d5
ascndia <fixgmc@gmail.com>
2026-05-31 21:30:19 +0700

    pr pertemuan 12

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-    <title>PWEB - Pensil Management</title>
+    <title>5026241093 - Abdurrahman Izzuddin Al Faruq</title>
 
     <meta name="description" content="" />
     <meta name="keywords" content="" />
